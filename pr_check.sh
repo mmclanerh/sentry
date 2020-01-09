e93b348e4faf528e85cb49664e7ff1f17f7d2bcb
Rob Rati <rrati@redhat.com>
2020-01-09 15:50:32 -0500

    pr_check will process templates (#21)

--- pr_check.sh
+++ pr_check.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-exit 0
+oc process --local -f sentry.yaml | oc apply --dry-run -f -
+rc=$?
+if [ $rc -ne 0 ]
+then
+  exit $rc
+fi
+
+oc process --local -f init-db.yaml | oc apply --dry-run -f -
